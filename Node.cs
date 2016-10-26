278946ce525403f60a657c970a1d2fdb52542e6b
Rodrigo36 <rodrigosg36@gmail.com>
2016-10-26 16:09:17 +1100

    Initial Commit

---
+++ Node.cs
@@ -0,0 +1,27 @@
+﻿namespace Snake
+{
+    /// <summary>
+    /// Node class used to build snake and other items.
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// X location
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Y location
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Node()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
